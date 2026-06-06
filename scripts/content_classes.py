86b2069865d4a9aaadf9aa206e505f9387759fa3
reallaksh19 <reallaksh19@gmail.com>
2026-06-06 08:29:59 +0400

    phase1: add content class registry shell

---
+++ scripts/content_classes.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+NEWS_CLASSES = set()
+INSIGHT_CLASSES = set()
+ACTIONABLE_CLASSES = set()
+UTILITY_CLASSES = set()
+DERIVED_CLASSES = set()
+KNOWN_CONTENT_CLASSES = frozenset()
